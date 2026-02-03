a337c62a7c3f665eb1744f3cf5bfede674071280
Chayut66070504005 <chayut2548@gmail.com>
2026-02-03 19:48:14 +0700

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from datetime import timezone
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(unique=True)
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.user_id} - {self.user_name}"
+
+class Store(models.Model):
+    store_id = models.IntegerField(unique=True)
+    store_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.store_id} - {self.store_location}"
+    
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
---
+++ api/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .serializers import StoreSerializer, ProductSerializer, UserSerializer
+from .models import Store, Product, User
+from rest_framework import generics, response, status
+
+# Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "user_id"
+
+class UserDeleteAll(generics.DestroyAPIView):
+    def delete(self, request,args, **kwargs):
+        User.objects.all().delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+class StoreList(generics.ListCreateAPIView):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+
+
+class StoreDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+    lookup_field = 'store_id'
+
+
+class StoreDeleteAll(generics.DestroyAPIView):
+    def delete(self, request, *args, **kwargs):
+        # Delete all Store objects
+        Store.objects.all().delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+    
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
